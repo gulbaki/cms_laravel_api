3380689ac241a21a4481d0c9a5886cc9d38c076e
DESKTOP-MO9U35H\baki-gul <baki.gul@armagroupholding.com>
2022-02-07 08:42:58 +0300

    try catch blog added

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -12,16 +12,24 @@ class AuthController extends BaseController
 {
     public function signin(Request $request)
     {
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
-            $authUser = Auth::user(); 
-            $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken; 
-            $success['name'] =  $authUser->name;
-   
-            return $this->sendResponse($success, 'User signed in');
-        } 
-        else{ 
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
-        } 
+      
+        try {
+            if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+           
+                $authUser = Auth::user(); 
+                $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken; 
+                $success['name'] =  $authUser->name;
+       
+                return $this->sendResponse($success, 'User signed in');
+            } 
+            else{ 
+                return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+            } 
+        } catch (\Exception $e) {
+            print($e->getMessage());
+        }
+
+      
     }
     public function signup(Request $request)
     {
--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'signin']);
 Route::post('register', [AuthController::class, 'signup']);
-     
+
 Route::middleware('auth:sanctum')->group( function () {
    
     Route::resource('blogs', BlogController::class);
